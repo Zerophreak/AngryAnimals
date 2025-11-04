90e03bf5ae2800b5fc29dd00ebdda806bdd19abb
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-11-04 11:43:14 +0100

    updating level scene

---
+++ AngryAnimals/Scenes/Levels/Level.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Level : Node2D
+{
+	[Export] private Marker2D _animalSpawn;
+	[Export] private PackedScene _animalScene;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+		SignalManager.Instance.OnAnimalDied += OnAnimalDied;
+		OnAnimalDied();
+    }
+
+    private void OnAnimalDied()
+    {
+		Animal newAnimal = _animalScene.Instantiate<Animal>();
+		newAnimal.Position = _animalSpawn.Position;
+		AddChild(newAnimal);
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    public override void _ExitTree()
+    {
+		SignalManager.Instance.OnAnimalDied -= OnAnimalDied;
+    }
+
+}
