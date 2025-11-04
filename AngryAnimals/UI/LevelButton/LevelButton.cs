77434b9b0fcf74a74b4f517cb4d27fdec23fa690
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-11-04 14:10:58 +0100

    Adding Ui Levelbutton and main scene

---
+++ AngryAnimals/UI/LevelButton/LevelButton.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class LevelButton : TextureButton
+{
+
+	static readonly Vector2 HOVER_SCALE = new Vector2(1.1f, 1.1f);
+	static readonly Vector2 NORMAL_SCALE = new Vector2(1.0f, 1.0f);
+
+	[Export] private int _levelNumber { get; set; }
+	[Export] private Label _levelLabel;
+	[Export] private Label _scoreLabel;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+		_levelLabel.Text = _levelNumber.ToString();
+		_scoreLabel.Text = ScoreManager.GetLevelBestScore().ToString("D4");
+
+		MouseEntered += OnMouseEntered;
+		MouseExited += OnMouseExited;
+		Pressed += OnPressed;
+	}
+
+    private void OnPressed()
+    {
+		GD.Print($"Selected: {_levelNumber}.");
+		ScoreManager.SetLevelSelected(_levelNumber);
+		GetTree().ChangeSceneToFile($"res://Scenes/Levels/Level{_levelNumber}.tscn");
+	}
+
+    private void OnMouseExited()
+    {
+		Scale = NORMAL_SCALE;
+    }
+
+
+    private void OnMouseEntered()
+    {
+		Scale = HOVER_SCALE;
+    }
+
+}
