37b07f6acc1c71d8a4a0a02fc573cc8417a01c99
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-11-10 15:13:37 +0100

    Updating signal manager adding signal
    uploaded wrong project signal manager

--- AngryAnimals/Globals/SignalManager.cs
+++ AngryAnimals/Globals/SignalManager.cs
@@ -3,10 +3,13 @@ using System;
 
 public partial class SignalManager : Node
 {
-	public static SignalManager Instance { get; private set; }
+	[Signal] public delegate void OnAnimalDiedEventHandler();
+	[Signal] public delegate void OnCupDestroyedEventHandler();
+	[Signal] public delegate void OnpressedEventHandler();
+	[Signal] public delegate void OnLevelCompleteEventHandler();
+	[Signal] public delegate void OnAttemptMadeEventHandler();
 
-	[Signal] public delegate void OnPlaneDiedEventHandler();
-	[Signal] public delegate void OnScoredEventHandler();
+	public static SignalManager Instance { get; private set; }
 
 
 	// Called when the node enters the scene tree for the first time.
@@ -15,13 +18,26 @@ public partial class SignalManager : Node
 		Instance = this;
 	}
 
-	public static void EmitOnPlaneDied()
+	public static void EmitOnAnimalDied()
+    {
+		Instance.EmitSignal(SignalName.OnAnimalDied);
+    }
+
+	public static void EmitOnCupDestroyed()
 	{
-		Instance.EmitSignal(SignalName.OnPlaneDied);
+		Instance.EmitSignal(SignalName.OnCupDestroyed);
 	}
 
-    public static void EmitOnScored()
-    {
-		Instance.EmitSignal(SignalName.OnScored);
+	public static void EmitOnLevelComplete()
+	{
+		Instance.EmitSignal(SignalName.OnLevelComplete);
 	}
+
+	public static void EmitonAttemptMade()
+	{
+		Instance.EmitSignal(SignalName.OnAttemptMade);
+	}
+
+	
+
 }
