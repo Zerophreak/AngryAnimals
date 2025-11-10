d616660d4c08065cda3c8125265196f8a2fdde80
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-11-10 15:16:43 +0100

    updating cup for signal

--- AngryAnimals/Scenes/Cup/Cup.cs
+++ AngryAnimals/Scenes/Cup/Cup.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Cup : StaticBody2D
 {
+	public const string GROUP_NAME = "Cup";
 	[Export] private AnimationPlayer _animationPlayer;
 
 	// Called when the node enters the scene tree for the first time.
