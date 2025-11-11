a44d7824b9c531fa6c5bcf6184ab8c9b99fa6b48
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2025-11-11 16:42:33 +0100

    updating scoremanager adding score persistence

--- AngryAnimals/Globals/ScoreManager.cs
+++ AngryAnimals/Globals/ScoreManager.cs
@@ -1,13 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class ScoreManager : Node
 {
 	const int DEFAULT_SCORE = 1000;
+	const string SCORE_FILE = "user://animal.save";
 
 	public static ScoreManager Instance { get; private set; }
-
+	
 	private int _levelSelected;
+	List<LevelScore> _levelScores = new List<LevelScore>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -24,9 +27,36 @@ public partial class ScoreManager : Node
 	{
 		return Instance._levelSelected = Level;
 	}
+
+	public static  LevelScore GetLevelScore(int levelNumber)
+	{
+		return Instance._levelScores.FirstOrDefault(ls => ls.LevelNumber == levelNumber);
+	}
 	
-	public static int GetLevelBestScore()
+	public static int GetLevelBestScore(int levelNumber)
     {
+		LevelScore LevelScore = GetLevelScore(levelNumber);
+		if(LevelScore != null)
+		{
+			return LevelScore.BestScore;
+		}
 		return DEFAULT_SCORE;
     }
+
+	public static void SetScoreForLevel(int levelNumber, int score)
+	{
+		LevelScore LevelScore = GetLevelScore(levelNumber);
+		if(LevelScore != null)
+		{
+			if(score < LevelScore.BestScore)
+			{
+				LevelScore.BestScore = score;
+				LevelScore.DateSet = DateTime.Now;
+			}
+		}
+		else
+		{
+			Instance._levelScores.Add(new LevelScore(levelNumber, score));
+		}
+	}
 }
